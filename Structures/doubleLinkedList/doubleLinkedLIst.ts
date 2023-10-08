ceb3c537c8b580ff36f20b1e2c6266a354561f11
Juan Medina <64394556+Lisbrun@users.noreply.github.com>
2023-10-08 16:52:46 -0500

    Feature
    DoubleLinkedList

--- Structures/doubleLinkedList/doubleLinkedLIst.ts
+++ Structures/doubleLinkedList/doubleLinkedLIst.ts
@@ -44,4 +44,88 @@ export class DoubleLinkedList<T> {
   Size (): number {
     return this.size
   }
+
+  change (startPosition: number, finalPosition: number): void {
+    if (this.isEmpty()) {
+      throw new Error('Double LinkedList is empty')
+    } else if (
+      startPosition < 0 ||
+      finalPosition < 0 ||
+      startPosition >= this.size ||
+      finalPosition >= this.size ||
+      startPosition === finalPosition
+    ) {
+      throw new Error('Invalid position')
+    }
+
+    let currentNode = this.head // Nodo actual que queremos encontrar
+    let currentIndex = 0
+    let nodeToMove: Node<T> | null = null
+
+    while (currentNode !== null && currentIndex < startPosition) {
+      currentNode = currentNode.next
+      currentIndex++
+    }
+
+    if (currentNode === null) {
+      throw new Error('Invalid start position')
+    }
+
+    nodeToMove = currentNode
+
+    if (currentNode.prev !== null) {
+      currentNode.prev.next = currentNode.next
+    } else {
+      this.head = currentNode.next
+    }
+
+    if (currentNode.next !== null) {
+      currentNode.next.prev = currentNode.prev
+    } else {
+      this.tail = currentNode.prev
+    }
+
+    currentNode = this.head
+    currentIndex = 0
+
+    while (currentNode !== null && currentIndex < finalPosition) {
+      currentNode = currentNode.next
+      currentIndex++
+    }
+
+    if (currentNode === null) {
+      this.tail = nodeToMove
+      nodeToMove.next = null
+      nodeToMove.prev = this.tail
+      this.tail.next = nodeToMove
+    } else {
+      nodeToMove.next = currentNode
+      nodeToMove.prev = currentNode.prev
+
+      if (currentNode.prev !== null) {
+        currentNode.prev.next = nodeToMove
+      } else {
+        this.head = nodeToMove
+      }
+
+      currentNode.prev = nodeToMove
+    }
+  }
+
+  deleteEnd (): T | null {
+    if (this.isEmpty()) {
+      return null
+    } else {
+      if (this.tail === null) throw new Error('Double LinkedList is empty')
+      const value = this.tail.value
+      this.tail = this.tail.prev
+      if (this.tail !== null) {
+        this.tail.next = null
+      } else {
+        this.head = null
+      }
+      this.size--
+      return value
+    }
+  }
 }
