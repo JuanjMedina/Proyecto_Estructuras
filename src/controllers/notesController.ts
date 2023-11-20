438b0211e201bc97a6f727877c670dc153f912ff
jujgomezru <jujgomezru@unal.edu.co>
2023-11-20 01:14:02 -0500

    Prueba petición 1

--- src/controllers/notesController.ts
+++ src/controllers/notesController.ts
@@ -1,6 +1,6 @@
 import { NextFunction, Request as ExpressRequest, Response } from 'express'
 import { ConjuntoDisjunto } from '../../Structures/ConjuntosDisjuntos/Disjoinset'
-// import { AVLTree } from '../../Structures/AVL/Avl'
+import { AVLTree } from '../../Structures/AVL/Avl'
 
 interface Request extends ExpressRequest {
   user?: any
@@ -48,37 +48,36 @@ export class NotesController {
           dataObject.id_nota,
           dataObject.id_carpeta
         )
+        console.log(dataObject)
       }
-      console.log(conjuntoDisjunto)
+      // console.log(conjuntoDisjunto)
       const result = await this.notesModel.updateNoteandFolder({
         //! falta logica
         dataNoteandFolder: req.body
       })
       conjuntoDisjunto.cambiarCarpetaDeNota(idNota, idCarpeta)
-      console.log(conjuntoDisjunto)
+      // console.log(conjuntoDisjunto)
       res.status(200).json(result)
     } catch (e) {
       res.status(400).json({ message: 'error' })
     }
   }
 
-  // findNoteById = async (req: Request, res: Response): Promise <void> => {
-  //   try {
-  //     const { idNota } = req.body
-  //     const FolderandNotes = await this.notesModel.getAllNotesandFolders()
-  //     const comparadorNotas = (a: Notes, b: Notes): number => {
-  //       if (a.idNota < b.idNota) return -1
-  //       else if (a.idNota > b.idNota) return 1
-  //       return 0
-  //     }
-  //     const AVLTree = new AVLTree<number>(comparadorNotas)
-  //     for (const dataObject of FolderandNotes) {
-  //       AVLTree.insert(dataObject)
-  //     }
-  //     const result = AVLTree.search(idNota)
-  //     res.status(200).json(result)
-  //   } catch (e) {
-  //     res.status(400).json({ message: 'la embarraste jeronimo' })
-  //   }
-  // }
+  findNoteById = async (req: Request, res: Response): Promise <void> => {
+    try {
+      const { idNota } = req.body
+      const FolderandNotes = await this.notesModel.getAllNotesandFolders()
+      const comparadorIds = (a: number, b: number): number => {
+        return a - b
+      }
+      const AVL = new AVLTree<number>(comparadorIds as (a: number, b: number) => number)
+      for (const dataObject of FolderandNotes) {
+        AVL.insert(dataObject.id_nota)
+      }
+      const result = AVL.search(idNota)
+      res.status(200).json(result)
+    } catch (e) {
+      res.status(400).json({ message: 'la embarraste jeronimo' })
+    }
+  }
 }
